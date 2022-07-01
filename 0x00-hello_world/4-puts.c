08b2df4883e60dcd489623ac42376687e16e64ae
Tingo92 <ngaraimba@gmail.com>
2022-06-30 17:43:43 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+/**
+ *
+ *  *main - entry point
+ *
+ *   *
+ *
+ *    *Return: 0 after printing the function
+ *
+ *     */
+
 int main(void)
+
 {
-	puts("Programming is like building a multilingual puzzle\n");
+
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
