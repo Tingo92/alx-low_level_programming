3a63863e641c64a2cf323c076b2319509c6bca47
Tingo92 <ngaraimba@gmail.com>
2022-07-03 07:50:27 -0700

    C Commits

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * main: 'Dora Korpar
+ *
+ *Return 0 (Successful)
+ *
+ **/
+int main(void)
+{
+	write("\"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19");
+	Return 0;
+}
